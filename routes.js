20dc9d2e5994f153e1d4777a72aeab73496eb55b
Rafkev <tolakins@gmail.com>
2023-12-09 10:10:57 -0800

    Updated README with setup instructions

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
 Temporary update at 04/26/2024 14:40:19 for commit 10
 Temporary update at 05/27/2024 10:17:03 for commit 13
 Temporary update at 02/19/2024 17:12:55 for commit 28
+Temporary update at 12/09/2023 10:10:57 for commit 71
