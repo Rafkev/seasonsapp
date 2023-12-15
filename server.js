735f6f17d0fd155c0d63758ba02ebddbb7e9ad64
Rafkev <tolakins@gmail.com>
2023-12-14 19:32:51 -0800

    Updated README with setup instructions

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ Temporary update at 02/03/2024 23:45:47 for commit 51
 Temporary update at 01/25/2024 23:01:31 for commit 81
 Temporary update at 04/22/2024 15:52:56 for commit 84
 Temporary update at 05/18/2024 13:04:06 for commit 175
+Temporary update at 12/14/2023 19:32:51 for commit 178
