ac12780dc2350574664fc7a55f7088e55f5feabc
Rafkev <tolakins@gmail.com>
2023-12-25 13:41:01 -0800

    Removed deprecated functions

--- utils.py
+++ utils.py
@@ -11,3 +11,4 @@ Temporary update at 01/20/2024 07:09:34 for commit 203
 Temporary update at 02/22/2024 02:42:19 for commit 208
 Temporary update at 05/15/2024 21:19:59 for commit 224
 Temporary update at 01/24/2024 00:09:25 for commit 234
+Temporary update at 12/25/2023 13:41:01 for commit 237
