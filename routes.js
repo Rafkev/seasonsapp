bf48e8021b187eafb91828f38de2f0924d2d6b33
Rafkev <tolakins@gmail.com>
2024-02-13 21:21:47 -0800

    Updated documentation for clarity

--- routes.js
+++ routes.js
@@ -4,3 +4,4 @@ Temporary update at 02/19/2024 17:12:55 for commit 28
 Temporary update at 12/09/2023 10:10:57 for commit 71
 Temporary update at 12/25/2023 16:10:37 for commit 82
 Temporary update at 03/29/2024 17:58:50 for commit 131
+Temporary update at 02/13/2024 21:21:47 for commit 135
