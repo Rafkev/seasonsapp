3b9945ddd6c350b251e63f5ade82efa283f6c0c9
Rafkev <tolakins@gmail.com>
2024-03-26 15:01:51 -0700

    Refactored backend service structure

--- database.sql
+++ database.sql
@@ -3,3 +3,4 @@ Temporary update at 12/03/2023 06:04:49 for commit 155
 Temporary update at 03/15/2024 17:34:24 for commit 158
 Temporary update at 04/03/2024 20:24:21 for commit 162
 Temporary update at 12/24/2023 03:17:54 for commit 198
+Temporary update at 03/26/2024 15:01:51 for commit 202
