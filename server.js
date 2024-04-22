5dd295082bc80940de9fe293503588456da4edaa
Rafkev <tolakins@gmail.com>
2024-04-22 15:52:56 -0700

    Updated documentation for clarity

--- server.js
+++ server.js
@@ -2,3 +2,4 @@ Temporary update at 02/08/2024 08:27:49 for commit 1
 Temporary update at 03/21/2024 06:13:35 for commit 48
 Temporary update at 02/03/2024 23:45:47 for commit 51
 Temporary update at 01/25/2024 23:01:31 for commit 81
+Temporary update at 04/22/2024 15:52:56 for commit 84
