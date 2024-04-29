1d2e31790a9bc7294710ef88267d274657e56d78
Rafkev <tolakins@gmail.com>
2024-04-29 03:12:26 -0700

    Updated README with setup instructions

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ Temporary update at 03/29/2024 21:37:26 for commit 31
 Temporary update at 03/28/2024 16:00:37 for commit 54
 Temporary update at 03/24/2024 00:22:56 for commit 88
 Temporary update at 04/26/2024 00:18:35 for commit 115
+Temporary update at 04/29/2024 03:12:26 for commit 140
