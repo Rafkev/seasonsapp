0e187747fa76a66c3526cad1df8e43b9dc1bf888
Rafkev <tolakins@gmail.com>
2024-05-18 13:04:06 -0700

    Added validation checks to input forms

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ Temporary update at 03/21/2024 06:13:35 for commit 48
 Temporary update at 02/03/2024 23:45:47 for commit 51
 Temporary update at 01/25/2024 23:01:31 for commit 81
 Temporary update at 04/22/2024 15:52:56 for commit 84
+Temporary update at 05/18/2024 13:04:06 for commit 175
