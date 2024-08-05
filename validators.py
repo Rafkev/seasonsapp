58f39d7675328cf0426a5a8124ea6f84ba65955f
Rafkev <tolakins@gmail.com>
2024-08-05 12:34:24 -0700

    Refactored code for better readability

--- validators.py
+++ validators.py
@@ -14,3 +14,4 @@ Temporary update at 05/10/2024 05:14:20 for commit 240
 Temporary update at 01/06/2024 20:00:54 for commit 258
 Temporary update at 02/01/2024 18:10:44 for commit 281
 Temporary update at 08/17/2024 17:10:38 for commit 65
+Temporary update at 08/05/2024 12:34:24 for commit 104
